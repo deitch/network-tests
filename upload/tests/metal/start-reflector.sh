6f4c0b8a4a711f50f2a97e4057a888bc4d0b5ee7
Avi Deitcher <avi@deitcher.net>
2016-06-10 13:12:30 +0300

    On metal, kill an old netserver

--- upload/tests/metal/start-reflector.sh
+++ upload/tests/metal/start-reflector.sh
@@ -6,5 +6,8 @@ COMMON=$(dirname "${BASH_SOURCE[0]}")/../../common
 . $COMMON/getoption
 
 
+# make sure an old one isn't running
+pkill netserver || true
+
 netserver -p $NETSERVERPORT >/dev/null
 pgrep netserver
