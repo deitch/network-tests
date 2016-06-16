96629d650aab913d44dac572ac1366bb4241387d
Avi Deitcher <avi@deitcher.net>
2016-06-16 10:36:26 +0300

    Remove -i from docker run for background sh process in bridgenonat for faster execution

--- upload/tests/bridgenonat/run-test.sh
+++ upload/tests/bridgenonat/run-test.sh
@@ -11,7 +11,7 @@ IP3=${PRIVATEIPS[2]}
 IP4=${PRIVATEIPS[3]}
 
 
-docker run -it -d --net=none --name=netperf netperf sh
+docker run -t -d --net=none --name=netperf netperf sh
 
 pid=$(docker inspect -f '{{ .State.Pid }}' netperf)
 
