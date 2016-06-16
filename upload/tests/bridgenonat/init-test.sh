dad1e53201556d6bcdd8b2b0619b3a0ea618f91a
Avi Deitcher <avi@deitcher.net>
2016-06-16 10:03:21 +0300

    Correctly handle per-server IP requests for bridgenonat

---
+++ upload/tests/bridgenonat/init-test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+COMMON=$(dirname "${BASH_SOURCE[0]}")/../../common
+. $COMMON/getoption
+
+IP1=${PRIVATEIPS[0]}
+IP2=${PRIVATEIPS[1]}
+IP3=${PRIVATEIPS[2]}
+IP4=${PRIVATEIPS[3]}
+
+
+docker run -it -d --net=none --name=netperf netperf sh
+
+pid=$(docker inspect -f '{{ .State.Pid }}' netperf)
+
+
+# before we set up the veth pair, make sure it does not exist
+if ip link show | grep -qw B0 ; then
+	ip link del B0
+fi
+ip link add B1 type veth peer name B0
+ip link set B0 master br0
+ip link set B0 up
+ip link set B1 netns $pid
+
+nsenter --target $pid --net ip link set dev B1 name eth0
+nsenter --target $pid --net ip link set eth0 up
+nsenter --target $pid --net ip addr add $IP4/29 dev eth0
+# default route is just to the default on the switch
+nsenter --target $pid --net ip route add default via $IP2 dev eth0
+
--- upload/tests/bridgenonat/start-reflector.sh
+++ upload/tests/bridgenonat/start-reflector.sh
@@ -16,14 +16,6 @@ docker run $PORTLINE --net=none -d --name=netserver netperf netserver -D -p $NET
 
 pid=$(docker inspect -f '{{ .State.Pid }}' netserver)
 
-mkdir -p /var/run/netns
-
-
-# before we try to make the link, make sure it does not exist
-if [[ -e /var/run/netns/$pid ]]; then
-	rm -f /var/run/netns/$pid
-fi
-ln -s /proc/$pid/ns/net /var/run/netns/$pid
 
 # before we set up the veth pair, make sure it does not exist
 if ip link show | grep -qw A0 ; then
@@ -33,9 +25,9 @@ ip link add A1 type veth peer name A0
 ip link set A0 master br0
 ip link set A0 up
 ip link set A1 netns $pid
-ip netns exec $pid ip link set dev A1 name eth0
-ip netns exec $pid ip link set eth0 up
-ip netns exec $pid ip addr add $IP3/29 dev eth0
+nsenter --target $pid --net ip link set dev A1 name eth0
+nsenter --target $pid --net ip link set eth0 up
+nsenter --target $pid --net ip addr add $IP3/29 dev eth0
 # default route is just to the default on the switch
-ip netns exec $pid ip route add default via $IP2 dev eth0
+nsenter --target $pid --net ip route add default via $IP2 dev eth0
 
