e8fb9d5b4db4ca300f547bfc2ecbd5d7b78659f7
Avi Deitcher <avi@deitcher.net>
2016-06-16 09:16:58 +0300

    Removed any dependencies on special IPs for bridge tests

--- upload/tests/bridge/get-reflector-ip.sh
+++ upload/tests/bridge/get-reflector-ip.sh
@@ -5,7 +5,12 @@ set -e
 COMMON=$(dirname "${BASH_SOURCE[0]}")/../../common
 . $COMMON/getoption
 
+# get our management IP
+nic=team0
+mgmt=$(ip addr show $nic | awk '/10\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]+/ {print $2}')
+mgmt=${mgmt%%/*}
+
 localIP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' netserver)
-remoteIP=${PRIVATEIPS[1]}
+remoteIP=$mgmt
 
 echo $localIP $remoteIP
--- upload/tests/bridge/teardown-network.sh
+++ upload/tests/bridge/teardown-network.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
 
-set -e
-
-# set up network with private IPs for host 
-# for net=bridge, simply put both addresses on lo
-# because of port mapping, it will work just fine
-
-COMMON=$(dirname "${BASH_SOURCE[0]}")/../../common
-. $COMMON/getoption
-
-IP1=${PRIVATEIPS[0]}
-IP2=${PRIVATEIPS[1]}
-
-# need to check each address
-if ip addr show lo | grep -wq $IP1 ; then
-	ip address del $IP1/32 dev lo
-fi
-
-if ip addr show lo | grep -wq $IP2 ; then
-	ip address del $IP2/32 dev lo
-fi
