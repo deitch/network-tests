a099a1ecd386b3a9e65e9a678d962b2390def58d
Avi Deitcher <avi@deitcher.net>
2016-06-22 09:48:42 +0300

    cleanly remove ip range from calico netwotk in calico-native

--- upload/tests/calico-native/teardown-network.sh
+++ upload/tests/calico-native/teardown-network.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+
 # only remove the network once, on the source
 if docker network ls | grep -wq calico; then
+	calicoctl pool remove $PRIVATEIPCIDR || true
 	docker network rm calico || true
 fi
 calicoctl node stop
