c6a668f9f530ae396f526282a5d7177aa11f362f
Avi Deitcher <avi@deitcher.net>
2016-09-26 10:48:16 +0300

    Update versions for flannel and weave

--- upload/tests/flannel-udp/install-software.sh
+++ upload/tests/flannel-udp/install-software.sh
@@ -6,8 +6,9 @@ FLANNEL=/usr/local/bin/flanneld
 
 # do we already have flannel?
 if [[ ! -e $FLANNEL ]]; then
-	RELEASE=flannel-0.5.5
-	curl -L https://github.com/coreos/flannel/releases/download/v0.5.5/$RELEASE-linux-amd64.tar.gz -o $RELEASE.tar.gz
+	VERSION=0.6.2
+	RELEASE=flannel-${VERSION}
+	curl -L https://github.com/coreos/flannel/releases/download/v${VERSION}/$RELEASE-linux-amd64.tar.gz -o $RELEASE.tar.gz
 	tar xzvf $RELEASE.tar.gz
 	cp $RELEASE/flanneld $FLANNEL
 	chmod +x $FLANNEL
--- upload/tests/weave-sleeve/install-software.sh
+++ upload/tests/weave-sleeve/install-software.sh
@@ -7,7 +7,7 @@ WEAVE=/usr/local/bin/weave
 if [[ ! -e $WEAVE ]]; then
 	# because of the tx=off rx=off bug, we have to use the latest snapshot that fixed this
 	# see https://github.com/weaveworks/weave/issues/2354
-	curl -L git.io/weave-snapshot -o $WEAVE
-	# curl -L git.io/weave -o $WEAVE
+	# now this one is fixed as of 1.6.0
+	curl -L https://github.com/weaveworks/weave/releases/download/v1.6.2/weave -o $WEAVE
 	chmod +x $WEAVE
 fi
