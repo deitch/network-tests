95b4664e1298916b28f8eb504627dbf8d2a28fb8
Avi Deitcher <avi@deitcher.net>
2016-09-27 11:11:55 +0300

    Fix flannel installation per post-0.6 changes

--- upload/tests/flannel-udp/install-software.sh
+++ upload/tests/flannel-udp/install-software.sh
@@ -6,10 +6,10 @@ FLANNEL=/usr/local/bin/flanneld
 
 # do we already have flannel?
 if [[ ! -e $FLANNEL ]]; then
-	VERSION=0.6.2
+	VERSION=v0.6.2
 	RELEASE=flannel-${VERSION}
-	curl -L https://github.com/coreos/flannel/releases/download/v${VERSION}/$RELEASE-linux-amd64.tar.gz -o $RELEASE.tar.gz
+	curl -L https://github.com/coreos/flannel/releases/download/${VERSION}/$RELEASE-linux-amd64.tar.gz -o $RELEASE.tar.gz
 	tar xzvf $RELEASE.tar.gz
-	cp $RELEASE/flanneld $FLANNEL
+	cp flanneld $FLANNEL
 	chmod +x $FLANNEL
-fi	
+fi
