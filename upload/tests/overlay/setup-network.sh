ac4c405a418d98ca8de9552ae7f2711f2933fc5a
Avi Deitcher <avi@deitcher.net>
2016-09-27 21:29:09 +0300

    Ensure overlay networks only are set up and torn down on source

--- upload/tests/overlay/setup-network.sh
+++ upload/tests/overlay/setup-network.sh
@@ -3,9 +3,6 @@
 set -e
 
 # remove any old network if it exists
-if docker network ls | grep -wq overlay; then
-	docker network rm overlay
-fi
 
 IPRANGE=192.168.0.0/16
 hostname=$(hostname)
@@ -14,8 +11,11 @@ devtype=${hostname%%[0-9]*}
 # open the firewall ports necessary
 firewall-cmd --zone=trusted --add-source=$IPRANGE || true
 
-# only create the network once, on the source
+# only remove and create the network once, on the source
 if [[ "$devtype" == "source" ]]; then
+	if docker network ls | grep -wq overlay; then
+		docker network rm overlay
+	fi
 	docker network create --driver=overlay --subnet=$IPRANGE overlay
 fi
 
